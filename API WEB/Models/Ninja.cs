6622793d0c9e610fa67a3887ad9341bd195414ae
1Cellu <danielcortes1499@gmail.com>
2022-02-26 22:38:24 -0300

    Village and Ninja updated

--- API WEB/Models/Ninja.cs
+++ API WEB/Models/Ninja.cs
@@ -1,11 +1,17 @@
-﻿namespace API_WEB.Models
+﻿using API_WEB.Models.Enuns;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_WEB.Models
 {
     public class Ninja
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public Cla? Cla { get; set; }
-        public Village? Village { get; set; }
+
+        [Column(Name = "village_id")]
+        public Village? Village { get; set; } //Anotação para lembrar de fazer uma anotação
+        public OrderGraduation Graduation { get; set; }
         public bool IsRenegate { get; set; }
         
     }
--- API WEB/Models/Village.cs
+++ API WEB/Models/Village.cs
@@ -6,7 +6,7 @@ namespace API_WEB.Models
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public OrderGraduation Graduation { get; set; }
+
         public OrderCountry Country { get; set; }
     }
 }
